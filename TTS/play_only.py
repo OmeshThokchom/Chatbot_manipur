5c0dbaada4334ae55981f7c4df8f4344cfed1a96
OmeshThokchom <thokchomomesh@gmail.com>
2025-10-11 22:21:27 +0530

    update piper tts play only

--- TTS/play_only.py
+++ TTS/play_only.py
@@ -1,12 +1,30 @@
 from piperTTS import PiperTTS
-
+import sounddevice as sd
+import wave
+import numpy as np
+import io
 
 tts = PiperTTS()
 
 while True:
     try:
         txt = input("enter the text here:")
-        tts.text_to_speech(txt)
+        audio_buffer = tts.text_to_speech(txt)
+        
+        if audio_buffer:
+            # Read the audio data from the buffer
+            audio_buffer.seek(0)
+            with wave.open(audio_buffer, 'rb') as wf:
+                samplerate = wf.getframerate()
+                data = wf.readframes(wf.getnframes())
+                
+                # Convert to numpy array
+                audio_data = np.frombuffer(data, dtype=np.int16)
+                
+                # Play the audio
+                sd.play(audio_data, samplerate)
+                sd.wait()
+
     except KeyboardInterrupt:
         break
     except Exception as e:
