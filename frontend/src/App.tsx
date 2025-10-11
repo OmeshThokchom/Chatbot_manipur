351a646df764315075061327751803f139250b4f
Raj2025-hub <drazk21@gmail.com>
2025-10-11 05:52:58 +0530

    fix header and sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,10 @@ function App() {
   return (
     <div className="app-root-layout"> {/* New wrapper div */}
       <Header /> {/* Render Header here */}
-      <div className="app-container">
-        <Sidebar />
-        <main className="main-content">
-          <ChatPage />
-        </main>
-      </div>
+      <Sidebar />
+      <main className="main-content">
+        <ChatPage />
+      </main>
     </div>
   );
 }
