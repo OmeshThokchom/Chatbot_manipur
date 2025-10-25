dd558186a2dcacd1bdec7c27f5f590ed34cb4ddd
OmeshThokchom <thokchomomesh@gmail.com>
2025-10-26 02:04:37 +0530

    add meitei tts

---
+++ TTS/meitei_TTS.py
@@ -0,0 +1,13 @@
+import requests, base64
+
+API = "https://6279ee401a9d.ngrok-free.app/tts"
+
+data = {
+    "prompt": "ꯅꯪꯒꯤ ꯃꯤꯡ ꯀꯔꯤ ꯀꯧꯒꯦ ꯑꯗꯨꯒꯥ ꯅꯪ ꯆꯥꯛ ꯆꯥꯔꯕꯣ",
+    "description": "male voice, clear tone, fast tempo"
+}
+r = requests.post(API, json=data)
+audio = base64.b64decode(r.json()["audio"])
+
+with open("tts_output.wav", "wb") as f:
+    f.write(audio)
