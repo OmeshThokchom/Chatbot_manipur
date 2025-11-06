fd312f579980c12f6a0418f41256f2d200cd57e1
OmeshThokchom <thokchomomesh@gmail.com>
2025-11-06 21:21:40 +0530

    nothing

--- frontend/src/components/VoiceChatOverlay.tsx
+++ frontend/src/components/VoiceChatOverlay.tsx
@@ -114,8 +114,6 @@ const VoiceChatOverlay: React.FC<VoiceChatOverlayProps> = ({ onClose, analyser, 
 
 
 
-
-
       mediaRecorderRef.current.onstop = () => {
         console.log('Recording stopped, chunks:', audioChunksRef.current.length);
         const audioBlob = new Blob(audioChunksRef.current, { type: mimeType });
